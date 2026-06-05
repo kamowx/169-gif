2fad261888ad41bc2a139f2df22c54552216727d
kamowx <257101047+kamowx@users.noreply.github.com>
2026-06-05 23:52:52 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+function fly1(){
+    $('.fly').animate({
+        left: '1500px',
+        transition: '10s'
+    });
+}
+setTimeout(fly1, 1000);
+
+function fly2(){
+    $('.fly3').animate({
+        left: '1600px',
+        transition: '10s'
+    });
+}
+setTimeout(fly2, 1000);
+
+function ob1(){
+    $('.ob1').animate({
+        left: '200px',
+        transition: '30s'
+    });
+}
+setTimeout(ob1, 1000);
+
+function ob2(){
+    $('.ob2').animate({
+        left: '100px',
+        transition: '30s'
+    });
+}
+setTimeout(ob2, 1000);
+
+function ob3(){
+    $('.ob3').animate({
+        left: '300px',
+        transition: '30s'
+    });
+}
+setTimeout(ob3, 1000);
+
+
